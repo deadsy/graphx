48888460147209c32e55f05c8e26b459a5e0d735
Jason Harris <jasonh@mistsys.com>
2016-11-18 09:29:15 -0800

    Auto-naming of nodes and edges. Added graph merging.

--- graph.py
+++ graph.py
@@ -8,8 +8,16 @@ class Error(Exception):
   """base class for exceptions in this module"""
   pass
 
-
 #------------------------------------------------------------------------------
+
+node_uuid = 0
+
+def get_node_id():
+  """return a uuid for the node"""
+  global node_uuid
+  x = node_uuid
+  node_uuid += 1
+  return x
 
 class NodeError(Error):
   """Node Errors"""
@@ -18,10 +26,22 @@ class NodeError(Error):
 
 class Node(object):
   """node object"""
-  def __init__(self, name):
-    self.name = name
+
+  def __init__(self):
+    """create a node"""
+    self.id = get_node_id()
+    self.name = 'n%d' % self.id
 
 #------------------------------------------------------------------------------
+
+edge_uuid = 0
+
+def get_edge_id():
+  """return a uuid for the edge"""
+  global edge_uuid
+  x = edge_uuid
+  edge_uuid += 1
+  return x
 
 class EdgeError(Error):
   """Edge Errors"""
@@ -30,30 +50,53 @@ class EdgeError(Error):
 
 class Edge(object):
   """edge object"""
-  def __init__(self, name, n0, n1):
-    self.name = name
+
+  def __init__(self, n0, n1):
+    """create an edge: n0 to n1"""
+    self.id = get_edge_id()
+    self.name = 'e%d' % self.id
     self.n0 = n0
     self.n1 = n1
 
 #------------------------------------------------------------------------------
 
+graph_uuid = 0
+
+def get_graph_id():
+  """return a uuid for the graph"""
+  global graph_uuid
+  x = graph_uuid
+  graph_uuid += 1
+  return x
+
 class Graph(object):
   """graph object"""
-  def __init__(self, name):
-    self.name = name
+
+  def __init__(self):
+    """create an empty graph"""
+    self.id = get_graph_id()
+    self.name = 'g%d' % self.id
     self.nodes = {}
     self.edges = {}
+
+  def merge(self, g):
+    """merge graph g with this graph"""
+    for e in g.edges.itervalues():
+      self.add_edge(e)
+    for n in g.nodes.itervalues():
+      if not self.nodes.has_key(n.name):
+        self.add_node(n)
 
   def add_node(self, node):
     """add a node to the graph"""
     if self.nodes.has_key(node.name):
-      raise NodeError('node of this name already in graph')
+      raise NodeError('node %s is already in the graph' % node.name)
     self.nodes[node.name] = node
 
   def add_edge(self, edge):
     """add an edge to the graph"""
     if self.edges.has_key(edge.name):
-      raise EdgeError('edge of this name already in graph')
+      raise EdgeError('edge %s is already in the graph' % edge.name)
     self.edges[edge.name] = edge
     # add the nodes if they aren't in the graph
     if not self.nodes.has_key(edge.n0.name):
@@ -61,14 +104,14 @@ class Graph(object):
     if not self.nodes.has_key(edge.n1.name):
       self.add_node(edge.n1)
 
-  def __str__(self):
-    """output a graphviz graph description"""
+  def gv_str(self):
+    """return a graphviz graph description"""
     s = []
-    s.append('digraph "%s" {' % self.name)
+    s.append('digraph %s {' % self.name)
     for _,node in self.nodes.iteritems():
-      s.append('  "%s";' % node.name)
+      s.append('  %s;' % node.name)
     for _,edge in self.edges.iteritems():
-      s.append('  "%s"->"%s";' % (edge.n0.name, edge.n1.name))
+      s.append('  %s->%s;' % (edge.n0.name, edge.n1.name))
     s.append('}')
     return '\n'.join(s)
 
--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import graph
 #------------------------------------------------------------------------------
 
 def star_graph(n):
-  """generate start graph n"""
-  g = graph.Graph('s%d' % n)
-  ni = graph.Node('i')
+  """generate star graph n"""
+  g = graph.Graph()
+  ni = graph.Node()
   for i in xrange(n):
-    n = graph.Node('e%d' % i)
-    e = graph.Edge('i_to_e%d' % i , ni, n)
+    n = graph.Node()
+    e = graph.Edge(ni, n)
     g.add_edge(e)
   return g
 
@@ -23,8 +23,13 @@ def star_graph(n):
 
 def main():
   """create a graph"""
-  g = star_graph(10)
-  print g
+
+  g = graph.Graph()
+  for k in range(5):
+    gs = star_graph(10)
+    g.merge(gs)
+
+  print g.gv_str()
 
 main()
 
